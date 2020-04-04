0a1eaccda941b8e0cdba8ba90b624dca12f938bd
ПК <89068414983@bk.ru>
2020-04-04 18:02:01 +0500

    4

--- 6.py
+++ 6.py
@@ -1,51 +1,26 @@
-# Проверка числа на простоту
-import unittest
-def prime_num(n):
-    if n == 1:
+# 1. проверка числа на простоту.
+from math import sqrt
+def num_simple(n):
+    if n <= 1:
         return False
-    for i in range(2, n + 1):
-        if n % i == 0 and i == n:
-            return True
-        elif n % i == 0 and i != n:
+    i = 2
+    while i <= sqrt(n):
+        if n % i == 0:
             return False
-# список всех делителей числа
-def get_divisors(n):
-    lst_divisors = []
+        i += 1
+    return True
+n = int(input('Введите натуральное число '))
+print('1) проверяем на простоту\n', num_simple(n))
+5
+def num_divisors(n):
+    result = []
     for i in range(1, n + 1):
         if n % i == 0:
-            lst_divisors.append(i)
-    return lst_divisors
-#Большой делитель числаdef get_max_prime(n):
-    lst_max_prime = []
-    lst_divs = get_divisors(n)
-    for i in lst_divs:
-        if prime_num(i):
-            lst_max_prime.append(i)
-    return max(lst_max_prime)
-class TestPrimeNum(unittest.TestCase):
-    def test_simple_1(self):
-        self.assertTrue(prime_num(101))
-    def test_simple_2(self):
-        self.assertFalse(prime_num(102))
-class TestGetDivisors(unittest.TestCase):
-    def test_simple_3(self):
-        self.assertEqual(get_divisors(50), [1, 2, 5, 10, 25, 50])
-class TestGetMaxPrime(unittest.TestCase):
-    def test_simple_1(self):
-        self.assertEqual(get_max_prime(37), 37)
-    def test_simple_2(self):
-        self.assertNotEqual(get_max_prime(997), 996)
-class TestPrimeNum(unittest.TestCase):
-    def test_simple_1(self):
-        self.assertTrue(prime_num(101))
-    def test_simple_2(self):
-        self.assertFalse(prime_num(102))
-import unittest
-class TestGetDivisors(unittest.TestCase):
-    def test_simple_3(self):
-        self.assertEqual(get_divisors(50), [1, 2, 5, 10, 25, 50])
-class TestGetMaxPrime(unittest.TestCase):
-    def test_simple_1(self):
-        self.assertEqual(get_max_prime(37), 37)
-    def test_simple_2(self):
-        self.assertNotEqual(get_max_prime(997), 996)
+            result.append(i)
+    return result
+print('2) Вывод списка делителей числа\n', num_divisors(n))
+def num_max_divisor(n):
+    if n == 1:
+        return None
+    return max(list(filter(num_simple, num_divisors(n))))
+print('3) Вывод самого большого простого делителя числа\n', num_max_divisor(n))
---
+++ 6_1.py
@@ -0,0 +1,9 @@
+import unittest
+class TestGetDivisors(unittest.TestCase):
+    def num_simple(n):
+        self.assertEqual(get_divisors(50), [1, 2, 5, 10, 25, 50])
+class TestGetMaxPrime(unittest.TestCase):
+    def num_divisors(n):
+        self.assertEqual(get_max_prime(37), 37)
+        def num_max_divisor(n):
+        self.assertNotEqual(get_max_prime(997), 996)
